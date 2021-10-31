07065f7c658d6630e5f418da617dacf646434502
Jason <jwrounds@usfca.edu>
2021-10-31 17:27:26 -0500

    Initial commit

---
+++ src/main/java/my/projects/recipereceipts/RecipeReceiptsApplication.java
@@ -0,0 +1,13 @@
+package my.projects.recipereceipts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeReceiptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeReceiptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/projects/recipereceipts/RecipeReceiptsApplicationTests.java
@@ -0,0 +1,13 @@
+package my.projects.recipereceipts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeReceiptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
